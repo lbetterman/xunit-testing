4aa5d6874dadb832a33be64214cc552408af05ae
wbetterman <wbetterman@unomaha.edu>
2021-06-27 15:34:05 -0500

    first test should now pass

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -5,6 +5,9 @@ namespace Prime.Services
     public class PrimeService
     {
 	public bool IsPrime(int candidate) {
+	    if (candidate == 1) {
+		return false;
+	    }
 	    throw new NotImplementedException("Not implemented.");
 	}
     }
